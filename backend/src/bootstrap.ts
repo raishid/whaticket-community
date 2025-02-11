35a8a9021cd1791c1621400fc89e72ef121c54e7
fededav <federicoelbroder@gmail.com>
2025-02-11 18:28:00 -0400

    update node env

--- backend/src/bootstrap.ts
+++ backend/src/bootstrap.ts
@@ -1,5 +1,18 @@
 import dotenv from "dotenv";
 
+let pathEnv = ".env";
+
+if (process.env.NODE_ENV === "test") {
+  pathEnv = ".env.test";
+}
+
+if (process.env.NODE_ENV === "coolify") {
+  pathEnv = "../../.env";
+}
+
+if (process.env.NDOE_ENV === "development") {
+  pathEnv = ".env.development";
+}
 dotenv.config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+  path: pathEnv
 });
