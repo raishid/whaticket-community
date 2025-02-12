f8de503367ea19d796cfc43c0dbfbbf862faab03
fededav <federicoelbroder@gmail.com>
2025-02-11 20:35:33 -0400

    update add config

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,8 +5,8 @@ module.exports = {
     charset: "utf8mb4",
     collate: "utf8mb4_bin"
   },
-  dialect: process.env.DB_DIALECT || "mysql",
-  timezone: "-03:00",
+  dialect: "mysql",
+  timezone: process.env.TZ,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
