5ae321e13e6bb412a960e2058bcd5fde7b39a1e7
fededav <67575679+raishid@users.noreply.github.com>
2025-02-12 10:34:02 -0400

    upodat test enviroment

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,9 @@
 const backendUrl = import.meta.env.VITE_BACKEND_URL as string;
 const closeTicketAuto = import.meta.env.VITE_CLOSE_TICKET_AUTO as string;
 
+console.log({
+  backendUrl,
+  closeTicketAuto,
+});
+
 export { backendUrl, closeTicketAuto };
