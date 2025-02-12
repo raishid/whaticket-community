5af8d793277f47d7a5ca317e9bf4904bbe789147
fededav <67575679+raishid@users.noreply.github.com>
2025-02-12 11:27:20 -0400

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,4 @@
 const backendUrl = import.meta.env.VITE_BACKEND_URL as string;
 const closeTicketAuto = import.meta.env.VITE_CLOSE_TICKET_AUTO as string;
 
-console.log({
-  backendUrl,
-  closeTicketAuto,
-});
-
 export { backendUrl, closeTicketAuto };
