4eb650e7fa5be2eca12842dc082d7f850f38d5a7
fededav <67575679+raishid@users.noreply.github.com>
2025-02-12 14:45:06 -0400

    Update wbot.ts

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -52,8 +52,7 @@ export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
           executablePath: process.env.CHROME_BIN || undefined,
           // @ts-ignore
           browserWSEndpoint: process.env.CHROME_WS || undefined,
-          args: args.split(" "),
-          userDataDir: "/tmp/wbot-profie"
+          args: args.split(" ")
         }
       });
 
